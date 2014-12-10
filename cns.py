457badc391420bb5d001e82e09a7103ade6ed3a7
Neil Gupta <neil@tabuleapp.com>
2014-12-10 12:51:14 -0600

    Updated cns to work with xbee 900's

--- cns.py
+++ cns.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python
+
 # Ann CNS Controller
 # Copyright Neil Gupta 2014
-
-from xbee import ZigBee
 import serial, time, json, requests, re
 
 #### CONFIGURE HERE ####
@@ -9,11 +9,13 @@ import serial, time, json, requests, re
 # Enter your full xbee address here
 CNS_ADDRESS = "\\x00\\x13\\xA2\\x00\\x40\\x68\\x2F\\xB6"
 # Enter where your xbee is connected. On Pi's with only 2 USB ports, the lower one is USB0 and the higher one is USB1
-SERIAL_PORT = '/dev/ttyUSB0'
+SERIAL_PORT = '/dev/ttyUSB0' # rPi
+# SERIAL_PORT = '/dev/tty.usbserial-A501FXLG' # mac
 # The default should be fine
 BAUD_RATE = 9600
 # Where is Ann server located? (Do NOT include the trailing slash)
 SERVER = "http://ann.metamorphium.com"
+# SERVER = "http://localhost:3000"
 
 #### END CONFIGURATION ####
 
@@ -21,31 +23,27 @@ ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
 get_url = SERVER + "/brains/" + CNS_ADDRESS + "/instructions"
 post_url = SERVER + "/brains/" + CNS_ADDRESS + "/senses"
 
-# Send incoming data to server for processing
-def message_received(data):
-  try:
-    addr = data['source_addr_long'].encode("hex")
-    addr = re.sub("(.{2})", "\\x\\1", addr, 0, re.DOTALL)
-    payload = {'sensor_addr': addr, 'payload': data['rf_data']}
-    r = requests.post(post_url, payload)
-
-xbee = ZigBee(ser, escaped=True, callback=message_received)
+print get_url
+ser.write('d')
 
 while True:
   try:
-    # Check for new instructions every 60 seconds
+    # Check for new instructions every 2 seconds
     r = requests.get(get_url)
     j = json.loads(r.content)
     if len(j) > 0:
       for k in j:
-        xbee.send("tx", data=k['content'] + "\n", dest_addr_long=k['address'].decode('string_escape'), dest_addr="\xff\xff")
-    time.sleep(60)
+        print k['content']
+        for c in k['content']:
+          ser.write(str(c))
+        # xbee.send("tx", data=k['content'] + "\n", dest_addr_long=k['address'].decode('string_escape'), dest_addr="\xff\xff")
+    time.sleep(2)
   except KeyboardInterrupt:
     break
 
 # halt() must be called before closing the serial
 # port in order to ensure proper thread shutdown
-xbee.halt()
+# xbee.halt()
 ser.close()
 
 # xbee.send("tx", data="b\n", dest_addr_long="\x00\x13\xA2\x00\x40\x68\x2E\xA4", dest_addr="\xff\xff")
